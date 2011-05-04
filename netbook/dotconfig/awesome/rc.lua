a5f241894a0edc87ff62e546913be374bd9e1b62
Sander Mathijs van Veen <sandervv@gmail.com>
2011-05-04 15:19:46 +0200

    Changed to local zenburn theme.

--- netbook/dotconfig/awesome/rc.lua
+++ netbook/dotconfig/awesome/rc.lua
@@ -14,8 +14,8 @@ require("vicious")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
--- beautiful.init("~/.config/awesome/themes/zenburn/theme.lua")
+-- beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init("~/.config/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminal"
